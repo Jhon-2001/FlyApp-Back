35d1cb0af75c10a3b2dfa94c24993e01396d75d6
Ripan <ionut.ripan@sfappworks.com>
2024-04-14 13:35:06 +0300

    base register

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import express, { json } from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+import { authRouter } from './src/routes/auth/auth.route.js';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+app.use(cors());
+app.use(express.json());
 
-// app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Successful response.");
 });
-app.use(json());
+
+// @@@ROUTES@@@
+
+app.use("/auth" , authRouter)
+
+
+
+
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
+
+
